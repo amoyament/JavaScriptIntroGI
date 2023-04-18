7469c5552422b32df5d50081bcec754116c40693
Aubrey Moya-Mendez Trotter <Aubrey@AdminisatorsMBP.lan>
2023-04-17 20:29:57 -0400

    VeryEasy challenge completed

---
+++ VeryEasy/VeryEasy.js
@@ -0,0 +1,12 @@
+//VeryEasy: Create two variables and assign a number to each. 
+
+const ageClayton = 28;
+const ageAubrey = 27;
+
+
+//Console log the difference between the numbers.
+    //To log the difference, I first need to define it.
+const differenceBetween = ageClayton - ageAubrey;
+
+//Example output: “The difference between 6 and 2 is 4”
+console.log('The difference between ', [ageClayton], ' and ', [ageAubrey], 'is ', [differenceBetween]);
