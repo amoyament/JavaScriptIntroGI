3772388dc9e2973423cd52290083a7d90d97aa86
Aubrey Moya-Mendez Trotter <Aubrey@AdminisatorsMBP.lan>
2023-04-17 21:24:14 -0400

    Easy challenge completed and VeryEasy challenge updated.

---
+++ Easy/Easy.js
@@ -0,0 +1,20 @@
+//EASY: Create two variables and assign a person’s name to each. 
+let firstPerson = 'Clayto';
+let secondPerson = 'Aubrey';
+
+
+
+
+//Console log a statement that says which name is shorter or longer, and by how many characters. Example output: “The name Thomas is longer than John by 2 characters”
+
+console.log('The name', secondPerson, 'is longer than', firstPerson, 'by', secondPerson.length - firstPerson.length, 'character(s).');
+
+//If we want to be able to change these variables and log those differences on the fly, we can use a conditional
+
+if (firstPerson.length > secondPerson.length) {
+    console.log('The name', firstPerson, 'is longer than', secondPerson, 'by', firstPerson.length - secondPerson.length, 'character(s).');
+} else if (firstPerson.length < secondPerson.length) {
+    console.log('The name', secondPerson, 'is longer than', firstPerson, 'by', secondPerson.length - firstPerson.length, 'character(s).');
+} else if (firstPerson.length = secondPerson.length) {
+    console.log('The name', firstPerson, 'and', secondPerson, 'are the same length.')
+}
--- VeryEasy/VeryEasy.js
+++ VeryEasy/VeryEasy.js
@@ -6,7 +6,12 @@ const ageAubrey = 27;
 
 //Console log the difference between the numbers.
     //To log the difference, I first need to define it.
+
 const differenceBetween = ageClayton - ageAubrey;
 
 //Example output: “The difference between 6 and 2 is 4”
-console.log('The difference between ', [ageClayton], ' and ', [ageAubrey], 'is ', [differenceBetween]);
+
+console.log('The difference between', ageClayton, 'and', ageAubrey, 'is', differenceBetween);
+
+//After more research, I realized that I can console log the difference between numbers without defining difference between.
+console.log('The difference between', ageClayton, 'and', ageAubrey, 'is', ageClayton - ageAubrey);
